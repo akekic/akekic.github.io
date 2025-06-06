e3040939f0cabeffbaa46112aea0f278bfc8bf9f
Armin Kekic <armin.kekic@mailbox.org>
2025-06-06 15:01:13 +0200

    Add Intervention Generalization paper at ICML 2025

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { highlight_style: "dracula", theme: "black" };
+  var slides = {highlight_style: "dracula", theme: "black"};
 
   // ns-hugo:/var/folders/8g/thz77ff57v72nfc8v5qq1qrh0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210127225730-4ac1c6aa1a40/assets/js/wowchemy-utils.js
   function fixMermaid() {
@@ -15,7 +15,7 @@
     }
   }
 
-  // <stdin>
+  // js/wowchemy-slides.js
   var enabledPlugins = [RevealMarkdown, RevealHighlight, RevealSearch, RevealNotes, RevealMath, RevealZoom];
   var isObject = function(o) {
     return o === Object(o) && !isArray(o) && typeof o !== "function";
